25ce126173906e48d8d2cb6c1edd001697c8dc8c
Tabcam <1499073363@qq.com>
2019-11-10 20:42:42 +0800

    环境配置与工资实体

---
+++ ims-parent/ims-component/src/main/java/org/ssr/ims/entity/salary.java
@@ -0,0 +1,29 @@
+package org.ssr.ims.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class salary {
+	private Integer id;
+	private Integer salaryYear;
+	private Integer salaryMonth;
+	private Double baseSalary;
+	private Double insurance;
+	private Double accumnlationFund;
+	private Double tax;
+	private Double achievements;
+	private Double reallySalary;
+	private Double empId;
+	
+	
+	
+
+}
